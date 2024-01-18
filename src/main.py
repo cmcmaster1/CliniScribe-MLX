9322b74a083088701811d2ae366aec91a5c2f2aa
Chris McMaster <mcmastc1@gmail.com>
2024-01-19 10:53:05 +1100

    optimizations

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication
-from main_window import MainWindow  # Assuming you named the file main_window.py
+from main_window import MainWindow
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
